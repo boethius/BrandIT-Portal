89b8498efec9c46109285dc6b15c812b92ebc29e
Sebastian Boëthius <sebastian@boethius.ch>
2012-05-24 11:29:59 +0200

    jQuery Header
    Added the CDN Link to jQuery 1.7.2 to the template header.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php echo $title ?> - Portalsite</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 </head>
 <body>
 	<h1>Portalseite</h1>
