082d6b4a4e84bb96c1de51777a506469d907b43d
Bruno-Ricardo <brunoricardo@hotmail.ch>
2012-10-04 17:47:37 +0200

    logo einbindung

--- application/views/templates/search.php
+++ application/views/templates/search.php
@@ -5,7 +5,7 @@
             	
                 <div id="header_content_holder">
                     <div id="logo"></div>
-                    <div id="slogan"><?=$portal['name'];?></div>
+                    <div id="slogan"><img src="/images/<?=$portal['name'];?>_logo.png" alt="<?=$portal['name'];?>" /></div>
                     <div id="search">
                     <select name="regions" id="regions">
                     	<option value=""><?=$i18n->get_value("allregions")?></option>
